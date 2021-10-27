f91cf5878619e4af44a67f8657819644731c8dc5
liupeng1204 <1016300127@qq.com>
2021-10-27 18:04:46 +0800

    test

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -109,28 +109,28 @@ module.exports = {
                 "text": "标签"
             }
         },
-        // "friendLink": [
-        //     {
-        //         "title": "午后南杂",
-        //         "desc": "Enjoy when you can, and endure when you must.",
-        //         "email": "1156743527@qq.com",
-        //         "link": "https://www.recoluan.com"
-        //     },
-        //     {
-        //         "title": "vuepress-theme-reco",
-        //         "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        //         "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        //         "link": "https://vuepress-theme-reco.recoluan.com"
-        //     }
-        // ],
+        "friendLink": [
+            {
+                "title": "午后南杂",
+                "desc": "Enjoy when you can, and endure when you must.",
+                "email": "1156743527@qq.com",
+                "link": "https://www.recoluan.com"
+            },
+            {
+                "title": "vuepress-theme-reco",
+                "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+                "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+                "link": "https://vuepress-theme-reco.recoluan.com"
+            }
+        ],
         "logo": "/logo.png",
         "search": true,
         "searchMaxSuggestions": 10,
         "lastUpdated": "Last Updated",
         "author": "刘鹏",
         "authorAvatar": "/avatar.png",
-        // "record": "xxxx",
-        // "startYear": "2019"
+        "record": "xxxx",
+        "startYear": "2019"
     },
     "markdown": {
         "lineNumbers": true

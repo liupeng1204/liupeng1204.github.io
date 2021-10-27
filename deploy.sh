33b576a4621ea41cd4e9877efd3fb39285390227
liupeng1204 <1016300127@qq.com>
2021-10-27 10:54:45 +0800

    delete travis-config-files

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,48 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-# 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run build
+# 打印当前的工作路径
+pwd
+remote=$(git config remote.origin.url)
 
-# 进入生成的文件夹
-cd dist
+echo 'remote is: '$remote
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+# 新建一个发布的目录
+mkdir gh-pages-branch
+cd gh-pages-branch
+# 创建的一个新的仓库
+# 设置发布的用户名与邮箱
+git config --global user.email "$GH_EMAIL" >/dev/null 2>&1
+git config --global user.name "$GH_NAME" >/dev/null 2>&1
 git init
+git remote add --fetch origin "$remote"
+
+echo 'email is: '$GH_EMAIL
+echo 'name is: '$GH_NAME
+echo 'sitesource is: '$siteSource
+
+# 切换gh-pages分支
+if git rev-parse --verify origin/gh-pages >/dev/null 2>&1; then
+    git checkout gh-pages
+    # 删掉旧的内容
+    git rm -rf .
+else
+    git checkout --orphan gh-pages
+fi
+
+# 把构建好的文件目录给拷贝进来
+cp -a "../${siteSource}/." .
+
+ls -la
+
+# 把所有的文件添加到git
 git add -A
-git commit -m 'deploy'
+git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+git push --force --quiet origin gh-pages
 
-# 如果发布到 https://<USERNAME>.github.io
- git push -f git@github.com:liupeng1204/liupeng.github.io.git master
+# 资源回收，删除临时分支与目录
+cd ..
+rm -rf gh-pages-branch
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
+echo "Finished Deployment!"

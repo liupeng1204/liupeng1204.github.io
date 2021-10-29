c6f018f4df558818c323ddb7f129cdb50189cd34
liupeng1204 <1016300127@qq.com>
2021-10-29 09:39:06 +0800

    avatar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -119,12 +119,12 @@ module.exports = {
             {
                 "title": "vuepress-theme-reco",
                 "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-                "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+                // "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
                 "link": "https://vuepress-theme-reco.recoluan.com"
             }, {
                 "title": "CircleCI",
                 "desc": "Continuous Integration and Deployment.",
-                "avatar": "/img/circle-ci.png",
+                // "avatar": "/img/circle-ci.png",
                 "link": "https://app.circleci.com/"
             }
         ],

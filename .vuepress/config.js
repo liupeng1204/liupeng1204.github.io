2fa9ceb0a6a705ec882c66e5e612cf09092a21d0
liupeng1204 <1016300127@qq.com>
2021-10-29 11:00:21 +0800

    details

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -99,7 +99,7 @@ module.exports = {
                     ]
                 },
                 // "",
-                "develop/",
+                // "develop/",
                 // "interview/"
             ]
         },
